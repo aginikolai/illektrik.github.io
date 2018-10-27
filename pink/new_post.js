716676c0920c4f1d96258cf46f6153e00827c755
Vittman <vittman@list.ru>
2018-10-27 18:23:14 +0300

    fix

--- pink/new_post.js
+++ pink/new_post.js
@@ -1,19 +1,25 @@
 const changeing = document.querySelector('.new_filters');
 const inputs = changeing.getElementsByTagName('input');
 const filter = document.querySelector('.new_post_filter');
+var red = 255;
+var blue = 255;
+var green = 255;
 console.log(inputs);
 
 function update () {
 	if (inputs.indexOf(this) == 0) {
-		filter.style.background = `rgba(${255 - this.value}, 255, 255, .15)`;
+		red = 255 - this.value;
+		filter.style.background = `rgba( ${red} , ${green}, ${blue}, .15)`;
 	} else if (inputs.indexOf(this) == 1) {
-		filter.style.background = `rgba(255, ${255 - this.value}, 255, .15)`;
+		green = 255 - this.value;
+		filter.style.background = `rgba( ${red} , ${green}, ${blue}, .15)`;
 	} else if (inputs.indexOf(this) == 2) {
-		filter.style.background = `rgba(255, 255, ${255 - this.value}, .15)`;
+		blue = 255 - this.value;
+		filter.style.background = `rgba( ${red} , ${green}, ${blue}, .15)`;
 	}
 };
 
 inputs.forEach = [].forEach;
 inputs.indexOf = [].indexOf;
-inputs.forEach( input => input.addEventListener('change', update) );
+inputs.forEach( input => input.addEventListener('change', update ) );
 inputs.forEach( input => input.addEventListener('mousemove', update) );
